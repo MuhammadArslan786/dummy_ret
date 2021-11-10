3c1f2cbf0667c58b6b4aac2932f41cca7806cbec
MuhammadArslan786 <muhammad.arslan1080@gmail.com>
2021-11-10 08:19:18 +0500

    Initial commit

---
+++ app/src/main/java/com/arslan6015/dummy_ret/JsonPlaceHolderApi.java
@@ -0,0 +1,13 @@
+package com.arslan6015.dummy_ret;
+
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface JsonPlaceHolderApi {
+
+    @GET("posts")
+    Call<List<Post>> getPosts();
+}
